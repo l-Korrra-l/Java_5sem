8753c8514592cceda98a30179286c5a5bcb67ae3
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2021-12-30 00:53:02 +0300

    tets, openapi

--- лабы/Lab_02-08/Employees/src/test/java/bstu/merh/employees/PageTest.java
+++ лабы/Lab_02-08/Employees/src/test/java/bstu/merh/employees/PageTest.java
@@ -1,7 +1,11 @@
 package bstu.merh.employees;
 
 import bstu.merh.employees.Exceptions.ControllerException;
+import bstu.merh.employees.repository.EmployeeRepository;
+import bstu.merh.employees.service.UserService;
 import org.apache.log4j.Logger;
+import org.hibernate.service.spi.ServiceException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.UnsupportedEncodingException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -28,13 +33,24 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class PageTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private UserService userService;
+
     private static final Logger logger = Logger.getLogger(PageTest.class);
+//    integr
     @Test
     public void loadMainPage() throws Exception {
         logger.info("running main page test");
         mockMvc.perform(get("/")).andDo(print()).andExpect(status().is2xxSuccessful()); }
 
+    @Test
+    public void LoadUser() throws Exception {
+        mockMvc.perform(post("/users")).andDo(print()).andExpect(status().is2xxSuccessful());
+    }
 
+    //comp
     @Test
     public void testControllerAdvice() throws Exception
     {
@@ -47,6 +63,23 @@ public class PageTest {
                     }
                 })
                 .andExpect(status().isBadRequest());
+    }
+    @Test
+    public void testRepo() throws ServiceException {
+        try {
+            Assert.assertTrue(employeeRepository.existsById(12L));
+        } catch (Exception e) {
+            throw new ServiceException(e.getMessage());
+        }
+    }
+
+    @Test
+    public void existsUserByLoginAndPassword() {
+        try {
+            Assert.assertTrue(userService.existsUserByUsernameAndPassword("admin","admin"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
