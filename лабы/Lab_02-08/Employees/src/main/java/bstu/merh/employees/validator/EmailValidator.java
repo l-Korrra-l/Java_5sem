36e0d3e29f1a959638ca49a353e0cf2fe802d4e2
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2022-01-02 17:07:52 +0300

    validator

---
+++ лабы/Lab_02-08/Employees/src/main/java/bstu/merh/employees/validator/EmailValidator.java
@@ -0,0 +1,26 @@
+package bstu.merh.employees.validator;
+
+import bstu.merh.employees.model.EmailForm;
+import bstu.merh.employees.model.User;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class EmailValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        EmailForm some=(EmailForm)target;
+        if(some.getId()<0){
+            errors.rejectValue("id","negative value");
+        }
+    }
+}
+
+
+//openapi
--- лабы/Lab_02-08/Employees/src/main/java/bstu/merh/employees/validator/EmployeeValidator.java
+++ лабы/Lab_02-08/Employees/src/main/java/bstu/merh/employees/validator/EmployeeValidator.java
@@ -1,23 +1,26 @@
 package bstu.merh.employees.validator;
 
+import bstu.merh.employees.dto.EmployeeRequest;
 import bstu.merh.employees.model.EmailForm;
+import bstu.merh.employees.model.Employee;
 import bstu.merh.employees.model.User;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
-public class CustomValidator implements Validator {
+public class EmployeeValidator implements Validator {
+
     @Override
-    public boolean supports(Class<?> aClass) {
-        return User.class.equals(aClass);
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-        EmailForm some=(EmailForm)target;
-        if(some.getId()<0){
-            errors.rejectValue("id","negative value");
+        EmployeeRequest some=(EmployeeRequest) target;
+        if(some.getAge()<0 || some.getAge() > 140){
+            errors.rejectValue("id","nonono");
         }
     }
 }
